aef02c16b95cd3f4ce40b7de2a78b68c7630a5fa
Limu123 <lisa.mueller47@gmail.com>
2014-10-15 17:00:37 +0200

    modify karma.conf

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -45,9 +45,10 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: [
-      'PhantomJS'
-    ],
+    // browsers: [
+    //   'PhantomJS'
+    // ],
+    browsers: ['Chrome'],
 
     // Which plugins to enable
     plugins: [
